0159deb04df6485e905ec19fbe87a1cfedefdabe
yChang0609 <ychang0609@gmail.com>
2025-09-17 22:01:09 +0800

    [PPO] output feature number

--- app/ppo/utils.py
+++ app/ppo/utils.py
@@ -72,6 +72,7 @@ class JEPAExtractor(BaseFeaturesExtractor):
         pooler_params: dict = {}
     ):
         self._num_patchs = (crop_size//patch_size) ** 2
+        self._num_output_feat = (num_frames // tubelet_size)//2
         self._num_frames = num_frames
         super().__init__(observation_space, features_dim)
         encoder =  video_vit.__dict__[model_name](
@@ -107,7 +108,7 @@ class JEPAExtractor(BaseFeaturesExtractor):
             observations = observations.view(B, 3, self._num_frames, H, W)
             # observations = observations.repeat_interleave(4, dim=2)
             x = normalize_tensor(observations)
-            x = self._encoder(x)[:, -self._num_patchs:]
+            x = self._encoder(x)[:, -self._num_output_feat * self._num_patchs:]
             x = F.layer_norm(x, (x.size(-1),))
             x = self._pooler(x).squeeze(1)
             x = self._out_linear(x)
